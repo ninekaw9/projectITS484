647045b6f285a413e00b031e2d2e33ed1eac3d39
ninekaw9 <ninekaw9hma2@hotmail.com>
2016-11-20 17:36:41 +0700

    still error

--- siit_equipment_borrow/index.js
+++ siit_equipment_borrow/index.js
@@ -7,57 +7,119 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  ListView,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight,
+  AlertIOS,
 } from 'react-native';
-import * as firebase from 'firebase';
+const firebase = require('firebase');
+const StatusBar = require('./components/StatusBar.js');
+const ActionButton = require('./components/ActionButton.js');
+const ListItem = require('./components/ListItem.js');
+const styles = require('./styles.js');
 // Initialize Firebase
-  var config = {
+  const firebaseConfig = {
     apiKey: "AIzaSyCsVLTT_5zWzeGn0f77xD97k8itJQbEOYU",
     authDomain: "siit-equipment-audit.firebaseapp.com",
     databaseURL: "https://siit-equipment-audit.firebaseio.com",
     storageBucket: "siit-equipment-audit.appspot.com",
     messagingSenderId: "178324219535"
   };
-  firebase.initializeApp(config);
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 export default class siit_equipment_borrow extends Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      })
+    };
+    this.itemsRef = this.getRef().child('items');
+  }
+
+  getRef() {
+    return firebaseApp.database().ref();
+  }
+
+  listenForItems(itemsRef) {
+    itemsRef.on('value', (snap) => {
+
+      // get children as an array
+      var items = [];
+      snap.forEach((child) => {
+        items.push({
+          title: child.val().title,
+          _key: child.key
+        });
+      });
+
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(items)
+      });
+
+    });
+  }
+
+  componentDidMount() {
+    this.listenForItems(this.itemsRef);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+
+        <StatusBar title="Grocery List" />
+
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this._renderItem.bind(this)}
+          enableEmptySections={true}
+          style={styles.listview}/>
+
+        <ActionButton onPress={this._addItem.bind(this)} title="Add" />
+
       </View>
+    )
+  }
+
+  _addItem() {
+    AlertIOS.prompt(
+      'Add New Item',
+      null,
+      [
+        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        {
+          text: 'Add',
+          onPress: (text) => {
+            this.itemsRef.push({ title: text })
+          }
+        },
+      ],
+      'plain-text'
     );
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  _renderItem(item) {
+
+    const onPress = () => {
+      AlertIOS.alert(
+        'Complete',
+        null,
+        [
+          {text: 'Complete', onPress: (text) => this.itemsRef.child(item._key).remove()},
+          {text: 'Cancel', onPress: (text) => console.log('Cancelled')}
+        ]
+      );
+    };
+
+    return (
+      <ListItem item={item} onPress={onPress} />
+    );
+  }
+
+}
 
 AppRegistry.registerComponent('siit_equipment_borrow', () => siit_equipment_borrow);
