15c8a2dfd9c258c979844e07e3eccdcf5723266f
ninekaw9 <ninekaw9hma2@hotmail.com>
2016-11-23 11:13:59 +0700

    aaa

---
+++ siit_equipment_borrow/ItemDetail.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+import {TouchableOpacity, AppRegistry, ListView, StyleSheet, Text, View, Image, TextInput} from 'react-native';
+import *  as firebase from 'firebase';
+class ItemDetail extends Component{
+
+}
+export default ItemDetail;
