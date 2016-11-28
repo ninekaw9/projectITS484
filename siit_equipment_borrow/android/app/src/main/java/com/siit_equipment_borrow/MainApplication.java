04cac5ac08562aa040ba0c8547080ef2cfd975a2
ninekaw9 <ninekaw9hma2@hotmail.com>
2016-11-28 23:05:12 +0700

    remove unused library

--- siit_equipment_borrow/android/app/src/main/java/com/siit_equipment_borrow/MainApplication.java
+++ siit_equipment_borrow/android/app/src/main/java/com/siit_equipment_borrow/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.eguma.barcodescanner.BarcodeScanner;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new BarcodeScanner(),
             new RCTCameraPackage()
       );
     }
